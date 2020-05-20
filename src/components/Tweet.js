50c12d8be7c8c8cc4d1e7f2bba73f2d0cd04f575
Revin Tanri <tanrirevin@gmail.com>
2020-05-20 21:11:10 +0800

    Add mapStateToProps func in Tweet & import formatTweet in utils/helpers

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { formatTweet } from '../utils/helpers'
 
 class Tweet extends Component {
     render() {
+        console.log(this.props)
         return(
             <div className="tweet">
-
+                
             </div>
         )
     }
 }
 
-export default connect()(Tweet)
+function mapStateToProps({authedUser, users, tweets}, { id }) {
+    const tweet = tweets[id]
+
+    return {
+        authedUser,
+        tweet: formatTweet(tweet, users[tweet.author], authedUser)
+    }
+}
+
+export default connect(mapStateToProps)(Tweet)
