34efa55878af30d10755e46581e1720da9907b98
Revin Tanri <tanrirevin@gmail.com>
2020-05-24 10:47:19 +0800

    At ./reducers/tweets.js - Add case TOGGLE_TWEET foor likes

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -7,6 +7,18 @@ export default function users (state ={}, action) {
                 ...state, 
                 ...action.tweets
             }
+        case TOGGLE_TWEET :
+            return {
+                ...state,
+                [action.id]: {
+                    ...state[action.id],
+                    likes: action.hasLiked === true
+                        //If they are already like the tweet
+                        ? state[action.id].likes.filter((uid)=> uid !== action.authedUser)
+                        //if they haven't like the tweet, add the authedUser to 'Likes' array
+                        : state[action.id].likes.concat([action.authedUser])
+                }
+            }
         default :
             return state
     }
