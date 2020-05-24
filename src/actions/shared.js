0bcedb780ac809cad9db880255add7cdee70e498
Revin Tanri <tanrirevin@gmail.com>
2020-05-24 10:03:37 +0800

    put dispatch(showLoading()) and dispatch(hideLoading())

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -8,11 +8,13 @@ const AUTHED_ID = 'tylermcginnis'
 
 export function handleInitialData() {
     return (dispatch) => {
+        dispatch(showLoading())
         return getInitialData()
         .then(({users, tweets}) => {
             dispatch(receiveUsers(users))
             dispatch(receiveTweets(tweets))
             dispatch(setAuthedUser(AUTHED_ID))
+            dispatch(hideLoading())
         })
     }
 }
