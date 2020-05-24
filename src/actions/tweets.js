800b1b8bf28fcbf8dd235fc4544e895a29992624
Revin Tanri <tanrirevin@gmail.com>
2020-05-24 10:32:04 +0800

    Add & Export func handleToggleTweet that 'dispatch' and '.catch' error

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -19,3 +19,15 @@ function toggleTweet({id, authedUser, hasLiked}){
     }
 }
 
+export function handleToggleTweet (info) {
+    return(dispatch) => {
+        dispatch(toggleTweet(info))
+        
+        return saveLikeToggle(info)
+            .catch((e) => {
+                console.warn('Error in handleToggleTweet: ', e)
+                dispatch(toggleTweet(info))
+                alert('The was an error liking the tweet. Try again')
+            })
+    }
+}
