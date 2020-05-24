35479738d36ed454a6b5ee3514a56fb79e76c43b
Revin Tanri <tanrirevin@gmail.com>
2020-05-24 10:01:37 +0800

    Import showLoading and hideLoading in /actions/shared.js

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,6 +2,7 @@ import { getInitialData } from '../utils/api'
 import { receiveUsers } from './users'
 import { receiveTweets } from './tweets'
 import { setAuthedUser } from './authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 const AUTHED_ID = 'tylermcginnis'
 
