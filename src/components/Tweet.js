f6b2d3822911fdff0bdfb1bee820418dd166acee
Revin Tanri <tanrirevin@gmail.com>
2020-05-25 19:38:41 +0800

    @components/Tweet - Add link 'ReplyingTo' in each tweets

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import { formatTweet, formatDate } from '../utils/helpers'
 import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline} from 'react-icons/ti/index'
 import { handleToggleTweet } from '../actions/tweets'
-import {Link} from 'react-router-dom'
+import {Link, withRouter } from 'react-router-dom'
 
 class Tweet extends Component {
     handleLike =(e) => {
@@ -19,7 +19,7 @@ class Tweet extends Component {
     }
     toParent = (e, id) => {
         e.preventDefault()
-        // todo: Redirect to parent Tweet.
+        this.props.history.push(`/tweet/${id}`)
     }
     render() {
         const {tweet} = this.props
@@ -80,4 +80,4 @@ function mapStateToProps({authedUser, users, tweets}, { id }) {
     }
 }
 
-export default connect(mapStateToProps)(Tweet)
+export default withRouter(connect(mapStateToProps)(Tweet))
