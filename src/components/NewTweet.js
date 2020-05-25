f29443487bdb38732ea9accde1ab91636007f55c
Revin Tanri <tanrirevin@gmail.com>
2020-05-25 12:17:18 +0800

    Add tweetleft

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,29 +1,35 @@
 import React, {Component} from 'react'
 
 class NewTweet extends Component {
-    state ={
-        text= '',
+    state = {
+        text: '',
     }
     
     handleChange = (e) => {
-        const text = e.target.value,
+        const text = e.target.value
+
         this.setState(() => ( {text} ))
     }
 
     handleSubmit = (e) => {
+        e.preventDefault()
+
         const {text} = this.state
+
         // todo: add New Tweet into the store
         console.log('New Tweet: ', text)
         this.setState(()=> ( {text:''} ))
     }
 
     render() {
-        const {text} = this.props
+        const {text} = this.state
+
+        const tweetLeft = 280 - text.length
 
         return (
             <div>
                 <h3 className='center'>Compose New Tweet</h3>
-                <form className='new-tweet' onSubmit>
+                <form className='new-tweet' onSubmit={this.handleSubmit}>
                     <textarea 
                         placeholder="What's happening?"
                         value={text}
@@ -31,14 +37,18 @@ class NewTweet extends Component {
                         className="textarea"
                         maxLength={280}
                     />
-                <button
-                    className='btn'
-                    type='submit'
-                    disabled={text===''} >
-                    Submit
-                </button>
-            </form>
-            
+                    {tweetLeft <= 100 && (
+                        <div className='tweet-length'>
+                            {tweetLeft}
+                        </div>
+                    )}
+                    <button
+                        className='btn'
+                        type='submit'
+                        disabled={text===''} >
+                        Submit
+                    </button>   
+                </form>
             </div>
         )
     }
