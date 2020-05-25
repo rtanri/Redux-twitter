2d967f1829054205baf8255a6bcf5590ab2c7b6f
Revin Tanri <tanrirevin@gmail.com>
2020-05-25 11:58:28 +0800

    Component/NewTweet.js - Create handleChange(e)

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,9 +1,19 @@
 import React, {Component} from 'react'
 
 class NewTweet extends Component {
+    state ={
+        text= '',
+    }
+    
+    handleChange = (e) => {
+        const text = e.target.value,
+
+        this.setState(() => ( {text} ))
+    }
+
     render() {
         return (
-            
+
         )
     }
 }
